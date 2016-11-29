00e1be399ac4aa020cb6c3f9003f295d0b374f0c
carmen-c <carmen.csmwc@gmail.com>
2016-11-29 11:17:43 -0800

    add gmspath property

--- Midterm/MapViewController.m
+++ Midterm/MapViewController.m
@@ -18,6 +18,7 @@
 
 @property (weak, nonatomic) IBOutlet GMSMapView *googleMapView;
 @property (nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) GMSPath *pathToDisplay;
 @end
 
 @implementation MapViewController
