ef2c2681e22e6170428942f02871c379d6ff5ee1
Suvan <suvanramani@gmail.com>
2016-11-29 12:19:51 -0800

    Add tableView logic to preferencesVC

--- Midterm/PreferencesViewController.m
+++ Midterm/PreferencesViewController.m
@@ -8,25 +8,61 @@
 
 #import "PreferencesViewController.h"
 
-@interface PreferencesViewController ()
+@interface PreferencesViewController () <UITableViewDelegate, UITableViewDataSource>
+
 @property (weak, nonatomic) IBOutlet UILabel *instructionsLabel;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (nonatomic, strong) NSArray *placesOfInterest;
+@property (nonatomic, strong) NSDictionary *placesOfInterest;
 @property (nonatomic, strong) NSMutableArray *selectedPlaces;
 @end
 
 @implementation PreferencesViewController
 
+static NSString * const kShowListOfDetoursIdentifier = @"showListOfDetours";
+static NSString * const kPlacesOfInterestCellIdentifier = @"placesOfInterestCell";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.placesOfInterest = @{@"Amusement Parks" : @"amusement_park",
+                              @"Aquariums" : @"aquarium",
+                              @"Art Galleries" : @"art_gallery",
+                              @"Campgrounds" : @"campground",
+                              @"Casinos" : @"casino",
+                              @"Lodging Services" : @"lodging",
+                              @"Museums" : @"museum",
+                              @"Parking" : @"parking",
+                              @"Malls" : @"shopping_mall",
+                              @"Zoos" : @"zoo"};
+}
+
+#pragma mark - TableView
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.placesOfInterest.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kPlacesOfInterestCellIdentifier];
+    
+    NSArray *arrayOfKeys = [self.placesOfInterest allKeys];
+    
+    cell.textLabel.text = arrayOfKeys[indexPath.row];
+    
+    return cell;
 }
 
 
+# pragma mark - Segues
 
 - (IBAction)findButton:(UIButton *)sender {
+    [self performSegueWithIdentifier:kShowListOfDetoursIdentifier sender:self];
 }
 
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:kShowListOfDetoursIdentifier]) {
+        // set places of interest on destination VC
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
