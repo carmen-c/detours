33e3045ecb168828112d127d55093e0ffb714478
carmen-c <carmen.csmwc@gmail.com>
2016-11-28 17:47:41 -0800

    2

--- Midterm/MapViewController.m
+++ Midterm/MapViewController.m
@@ -59,14 +59,80 @@
             dispatch_async(dispatch_get_main_queue(), ^{
                 GMSPath *path = [GMSPath pathFromEncodedPath:encodedPath];
                 GMSPolyline *polyline = [GMSPolyline polylineWithPath:path];
+                
                 polyline.map = self.googleMapView;
                 polyline.geodesic = YES;
                 polyline.strokeWidth = 5.f;
+                NSMutableArray *arrayOfCoordinates = [NSMutableArray array];
+                for (int i=0; i < path.count; i++) {
+                    CLLocationCoordinate2D coordinate = [path coordinateAtIndex:i];
+                    NSString *coordinateString = [NSString stringWithFormat:@"%ld,%ld", (long)coordinate.latitude, (long)coordinate.longitude];
+                    [arrayOfCoordinates addObject:coordinateString];
+                }
+                
             });
         }
         
     }];
 }
+                           
+
+//+ (void)polylineWithEncodedString:(NSString *)encodedString {
+//    const char *bytes = [encodedString UTF8String];
+//    NSUInteger length = [encodedString lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
+//    NSUInteger idx = 0;
+//    
+//    NSUInteger count = length / 4;
+//    CLLocationCoordinate2D *coords = calloc(count, sizeof(CLLocationCoordinate2D));
+//    NSUInteger coordIdx = 0;
+//    
+//    float latitude = 0;
+//    float longitude = 0;
+//    while (idx < length) {
+//        char byte = 0;
+//        int res = 0;
+//        char shift = 0;
+//        
+//        do {
+//            byte = bytes[idx++] - 63;
+//            res |= (byte & 0x1F) << shift;
+//            shift += 5;
+//        } while (byte >= 0x20);
+//        
+//        float deltaLat = ((res & 1) ? ~(res >> 1) : (res >> 1));
+//        latitude += deltaLat;
+//        
+//        shift = 0;
+//        res = 0;
+//        
+//        do {
+//            byte = bytes[idx++] - 0x3F;
+//            res |= (byte & 0x1F) << shift;
+//            shift += 5;
+//        } while (byte >= 0x20);
+//        
+//        float deltaLon = ((res & 1) ? ~(res >> 1) : (res >> 1));
+//        longitude += deltaLon;
+//        
+//        float finalLat = latitude * 1E-5;
+//        float finalLon = longitude * 1E-5;
+//        
+//        CLLocationCoordinate2D coord = CLLocationCoordinate2DMake(finalLat, finalLon);
+//        coords[coordIdx++] = coord;
+//        
+//        if (coordIdx == count) {
+//            NSUInteger newCount = count + 10;
+//            coords = realloc(coords, newCount * sizeof(CLLocationCoordinate2D));
+//            count = newCount;
+//        }
+//    }
+//    
+//    GMSMarker *marker = [[GMSMarker alloc] init];
+//    marker.position = *(coords);
+//    marker.snippet = @" ";
+//    marker.map = self.googleMapView;
+//
+//}
 
 
 #pragma mark - find current location
