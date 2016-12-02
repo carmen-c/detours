0340b03486869e82e04b2a7ff47a163ae5188340
Suvan <suvanramani@gmail.com>
2016-12-02 12:29:16 -0800

    Add documentation part 2

--- Midterm/CategoryContainer.h
+++ Midterm/CategoryContainer.h
@@ -7,10 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
-
+/**
+ * A model object used to store an array of detour objects of a particular place type
+ */
 @interface CategoryContainer : NSObject
 
+/// A string with the UI Version of a placetype (e.g. Art Galleries) of all the objects in the arrayOfRecommenations property of this object
 @property (nonatomic, strong) NSString *name;
+/// An array containing DetourPlace objects
 @property (nonatomic, strong) NSMutableArray *arrayOfRecommendations;
 
 -(instancetype)initWithName:(NSString *)name;
